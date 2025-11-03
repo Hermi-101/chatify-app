da267c0240e1706bd1533ab35dbda4aec00094ed
Hermi-101 <105059748+Hermi-101@users.noreply.github.com>
2025-11-03 22:32:53 +0300

    message-routes added

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,93 @@
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/Message.js";
+import User from "../models/User.js";
+
+export const getAllContacts = async(req, res) =>{
+    try{
+        const loggedInUserId = req.user._id;
+        // CORRECT
+const filteredUser = await User.find({
+    _id: { $ne: loggedInUserId }
+}).select('-password');
+return res.status(200).json(filteredUser);
+    }catch(error){
+        console.log("Error in getAllContact", error);
+        res.status(500).json({message:"Server error"});
+        
+    }
+
+}
+
+
+export const getMessagesByUserId = async (req, res) => {
+    try{
+        const myId =req.user._id;
+        const {id:userToChatId} = req.params
+
+        const message = await Message.find({
+            $or:[
+                {senderId:myId, receiverId: userToChatId},
+                {senderId:userToChatId, receiverId: myId},
+            ],
+
+        });
+
+        res.status(200).json(message)
+        
+    } catch(error){
+         console.log("Error in getAllContact", error);
+        res.status(500).json({message:"Server error"});
+    }
+}
+
+export const sendMessage =async (req, res) => {
+    try{
+        const {text, image} = req.body;
+        const {id:receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+
+        });
+        
+        await newMessage.save();
+
+        res.status(201).json(newMessage);
+
+    }catch(error){
+         console.log("Error in sendMesssage", error);
+        res.status(500).json({message:"Server error"});
+    }
+}
+
+export const getChatPartners = async (req, res) => {
+
+  try{
+    const loggedInUserId = req.user._id;
+    const messages = await Message.find({
+        $or:[{senderId: loggedInUserId},{receiverId:loggedInUserId}],
+    });
+
+    const chatPartnerIds = [...new Set(messages.map((msg) =>
+         msg.senderId.toString() === loggedInUserId.toString() 
+         ? msg.receiverId.toString() 
+         : msg.senderId.toString())
+    )];  
+
+    const chatPartners = await User.find({_id: {$in: chatPartnerIds}}).select("-password")
+
+    res.status(200).json(chatPartners)
+} catch(error){
+    console.log("Error in getAllContact", error);
+    res.status(500).json({message:"Server error"});
+}
+}
